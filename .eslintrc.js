733711609de3415e4153e563dea1130a0dcf9475
Matus Kello <m.kello1992@gmail.com>
2023-12-30 16:16:11 +0100

    added mui - pallete, eslint, i18next

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2], // Set your preferred indentation (e.g., 2 spaces)
+    quotes: ['error', 'single'], // Prefer single quotes
+    semi: ['error', 'always'], // Enforce semicolons at the end of statements
+    'no-var': 'error',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-fragments': 'error',
+    'react/jsx-no-undef': 'error',
+  },
+};
---
+++ src/configs/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFAB0D',
+      light: '#ffcf8a',
+      dark: '#33291b',
+    },
+    secondary: {
+      main: '#740001',
+      light: '#FF856E',
+      dark: '#4E0000',
+    },
+
+    tertiary: {
+      main: '#3A406B',
+      light: '#C7C8FC',
+      dark: '##031237',
+    },
+  },
+});
+
+export default theme;
