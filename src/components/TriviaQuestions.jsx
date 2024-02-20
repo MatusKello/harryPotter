24aa0e67af2042435bafb18487eb016aede5edc5
Matus Kello <m.kello1992@gmail.com>
2024-02-20 20:33:14 +0100

    updated trivia

--- src/components/TriviaQuestions.jsx
+++ src/components/TriviaQuestions.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Button,
-  Modal,
-  Typography,
-  List,
-  ListItem,
-  ListItemText,
-  Box,
-} from '@mui/material';
+import { Button, Modal, Typography, Box, Card } from '@mui/material';
 import trivia from '../Data/Trivia';
-import { useTranslation } from 'react-i18next';
 import Translation from './Translation';
+import { triviaOptionSwitch } from '../helpers';
 
 const TriviaQuestions = () => {
   const [open, setOpen] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answerStatus, setAnswerStatus] = useState(null); // State to track answer status
-  const { t } = useTranslation();
+  const [correctCount, setCorrectCount] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleBgColorChange = (index) => {
+    return selectedOption === index && answerStatus === true
+      ? 'green'
+      : answerStatus === false
+      ? 'red'
+      : 'white'; // Set background color based on answer status
+  };
 
   // Load last answered question index from localStorage
   useEffect(() => {
@@ -26,16 +27,23 @@ const TriviaQuestions = () => {
     }
   }, []);
 
-  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleReset = () => {
+    setCurrentQuestionIndex(0);
+    setCorrectCount(0);
+  };
 
-  const handleAnswer = (selectedOption) => {
+  const handleAnswer = (selectedOption, index) => {
+    setSelectedOption(index);
     const currentQuestion = trivia[currentQuestionIndex];
     const correctAnswer = currentQuestion.answer;
 
     // Check if the selected option matches the correct answer
     const isCorrect = selectedOption === correctAnswer;
 
+    if (isCorrect) {
+      setCorrectCount((prevCount) => prevCount + 1);
+    }
     // Set answer status
     setAnswerStatus(isCorrect);
 
@@ -51,7 +59,9 @@ const TriviaQuestions = () => {
 
   return (
     <div>
-      <Button onClick={handleOpen}>Start Trivia</Button>
+      <Button variant='contained' onClick={() => setOpen(true)}>
+        Start Trivia
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
@@ -63,23 +73,32 @@ const TriviaQuestions = () => {
             position: 'absolute',
             top: '50%',
             left: '50%',
-            transform: 'translate(-50%, -50%)',
-            bgcolor:
-              answerStatus === true
-                ? 'green'
-                : answerStatus === false
-                ? 'red'
-                : 'white', // Set background color based on answer status
             padding: '20px',
-            transition: 'background-color 0.5s', // Add transition for smoother effect
+            transform: 'translate(-50%, -50%)',
+            bgcolor: 'white',
           }}
         >
-          <Typography id='modal-title' variant='h6' component='h2'>
-            {`${currentQuestionIndex + 1} / ${trivia.length}`}
-          </Typography>
-          <Typography id='modal-title' variant='h6' component='h2'>
-            Trivia Question
-          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Typography id='modal-title' variant='h6' component='h2'>
+              Trivia Question
+            </Typography>
+            {currentQuestionIndex < trivia.length && (
+              <Typography id='modal-title' variant='h6' component='h2'>
+                {`Question ${currentQuestionIndex + 1}`}
+              </Typography>
+            )}
+            {currentQuestionIndex < trivia.length && (
+              <Typography id='modal-title' variant='h6' component='h2'>
+                {`${currentQuestionIndex + 1} / ${trivia.length}`}
+              </Typography>
+            )}
+          </Box>
           {currentQuestionIndex < trivia.length ? (
             <div>
               <Translation
@@ -88,24 +107,55 @@ const TriviaQuestions = () => {
                   sx: { color: (theme) => theme.palette.primary.main },
                 }}
               />
-              <List>
+              <>
                 {trivia[currentQuestionIndex].options.map((option, index) => (
-                  <ListItem
-                    button
+                  <Card
+                    sx={{
+                      my: 2,
+                      px: 4,
+                      py: 2,
+                      display: 'flex',
+                      alignItems: 'center',
+                      cursor: 'pointer',
+                      bgcolor: handleBgColorChange(index),
+                      transition: 'background-color 0.5s', // Add transition for smoother effect
+                    }}
                     key={index}
-                    onClick={() => handleAnswer(option)}
+                    onClick={() => handleAnswer(option, index)}
                   >
-                    <ListItemText primary={t(option)} />
-                  </ListItem>
+                    {triviaOptionSwitch(index)}
+                    <Translation
+                      text={option}
+                      typographyProps={{
+                        sx: { ml: 1 },
+                      }}
+                    />
+                  </Card>
                 ))}
-              </List>
+              </>
             </div>
           ) : (
-            <Typography id='modal-description'>
-              End of trivia questions.
-            </Typography>
+            <div>
+              <Typography
+                gutterBottom
+              >{`Correct answers: ${correctCount}`}</Typography>
+              <Typography id='modal-description'>
+                End of trivia questions.
+              </Typography>
+            </div>
           )}
-          <Button onClick={handleClose}>Close</Button>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Button onClick={handleClose}>Close</Button>
+            <Button variant='contained' onClick={handleReset}>
+              Try again
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </div>
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -28,3 +28,22 @@ export const houseSwitch = (house) => {
       return { title: 'Unknown', info: '' };
   }
 };
+
+export const triviaOptionSwitch = (index) => {
+  switch (index) {
+    case 0:
+      return 'A:';
+
+    case 1:
+      return 'B:';
+
+    case 2:
+      return 'C:';
+
+    case 3:
+      return 'D:';
+
+    default:
+      return '';
+  }
+};
